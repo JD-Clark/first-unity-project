729e2e70c5a2cfc5743e1534010caa103d40cb6e
Justin Clark <47698897+justclarkd@users.noreply.github.com>
2019-03-10 23:41:29 -0500

    Fixed the particle effects on collision with asteroids
    I found out that the size of the backgrounds and where I had the spawnner was causing my particle effects to be overlapped. I scaled down the size of the backgrounds to 1.5 and changed the starting X Position on space1 to 5 and space2 to 25. This allowed me to easy calculate the endX and startX for the repeating BG script to be consistent.

---
+++ Asteroid Space Runner v2/Assets/Player.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class Player : MonoBehaviour
+{
+
+    private Vector2 targetPos;
+    public float yIncrement = 4;
+
+    public float speed;
+
+    public float maxHeight;
+    public float minHeight;
+
+    public int health = 3;
+    public Text shipStatus;
+
+    public GameObject effect;
+    public GameObject gameOver;
+
+
+    private void Update()
+    {
+        
+        if (health <= 0)
+        {
+            gameOver.SetActive(true);
+            Destroy(gameObject);
+        }
+
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y < maxHeight)
+        {
+            targetPos = new Vector2(transform.position.x, transform.position.y + yIncrement);
+            transform.position = targetPos;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y > minHeight)
+        {
+            targetPos = new Vector2(transform.position.x, transform.position.y - yIncrement);
+            transform.position = targetPos;
+        }
+
+        if (health == 3)
+        {
+            shipStatus.text = "Status: Normal";
+        }
+        else if (health == 2)
+        {
+            shipStatus.text = "Status: Damaged";
+        }
+        else
+            shipStatus.text = "Status: Critical!";
+    }
+
+  
+    void OnTriggerEnter2D(Collider2D obstacle)
+    {
+       if (obstacle.CompareTag("Asteroid"))
+        {
+         Instantiate(effect, transform.position, Quaternion.identity);
+        }
+    }
+
+}
